b0aa3c9f356c9a6aba8f3af94987ed662f425d4c
Joseph Martire <joemart06@gmail.com>
2020-08-23 16:16:05 -0400

    Functions finish

--- src/App.js
+++ src/App.js
@@ -3,13 +3,70 @@ import './App.css';
 
 export default () => {
 
-const [arr, setArr] = useState(Array.from(Array(3), ()=> Array(3)))
+  const [arr, setArr] = useState(Array.from(Array(3), ()=> Array(3)))
+  const [doubleArr] = useState(['x','x','x','x','x','x','x','x','x'])
+  
+  //checks left and right
+  const toLeftRight = (arr, i) => {
+    return arr[i].every(x=>x==='x')
+  }
+  //checks up and down
+  const toUpDown = (arr, j) =>{
+    let temp = arr.map(a=>a[j])
+    return temp.every(x=>x==='x')
+  }
 
+  //checks top left towards bottom right
+  const fromTopLefttoBottomRight = (arr, i, j) =>{
+    //left() -> return all from the left
+    let temp = [];
+    (() =>{
+      let x = i
+      let y = j
+      while(arr[x] !== undefined && arr[x][y] !== undefined)
+      {
+        temp.push(arr[x--][y--])
+      }
+    })();
+    //right() -> return all from the right
+    (() =>{
+      let x = i
+      let y = j
+      while(arr[x+1] !== undefined && arr[x+1][y+1] !== undefined)
+      {
+        temp.push(arr[++x][++y])
+      }
+    })();
+    //compare strings and array size
+    return temp.every(x=>x===arr[i][j]) && temp.length == 3
+  }
 
-useEffect(()=>{
+  //checks top right towards bottom left
 
-  const doubleArr = 
-  [1,2,3,4,5,6,7,8,9]
+  const fromTopRighttoBottomLeft = (arr, i, j) =>{
+    //left() -> return all from the left
+    let temp = [];
+    (() =>{
+      let x = i
+      let y = j
+      while(arr[x] !== undefined && arr[x][y] !== undefined)
+      {
+        temp.push(arr[x++][y--])
+      }
+    })();
+    //right() -> return all from the right
+    (() =>{
+      let x = i
+      let y = j
+      while(arr[x-1] !== undefined && arr[x-1][y+1] !== undefined)
+      {
+        temp.push(arr[--x][++y])
+      }
+    })();
+    //compare strings and array size
+    return temp.every(x=>x===arr[i][j]) && temp.length == 3
+  }
+
 
   const double = a =>{
     let count = 0
@@ -21,11 +78,21 @@ useEffect(()=>{
     return a
   }
 
+  useEffect(()=>{
   setArr(a=>double(a))
-  console.log(arr)
-
-},[])
   
+  console.log(fromTopRighttoBottomLeft(arr,0,0))
+  console.log(fromTopRighttoBottomLeft(arr,1,0))
+  console.log(fromTopRighttoBottomLeft(arr,2,0))
+  console.log(fromTopRighttoBottomLeft(arr,0,1))
+  console.log(fromTopRighttoBottomLeft(arr,1,1))
+  console.log(fromTopRighttoBottomLeft(arr,2,1))
+  console.log(fromTopRighttoBottomLeft(arr,0,2))
+  console.log(fromTopRighttoBottomLeft(arr,1,2))
+  console.log(fromTopRighttoBottomLeft(arr,2,2))
+
+  console.log(arr)
+  },[])
 
   return (
   <>
